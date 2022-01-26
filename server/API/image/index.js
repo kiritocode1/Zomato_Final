21e633fb8cc9184622476f023ac98a46552a7a08
unknown <kathawalearyan9@gmail.com>
2022-01-26 13:57:32 +0530

    s3 development done :

--- server/API/image/index.js
+++ server/API/image/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import AWS from "aws-sdk";
 import multer from "multer";
 import { ImageModel } from "../../database/allModels";
+import { s3Upload } from "../../utils/AWS/s3";
+
 
 const Router = express.Router();
 //! configure multer
@@ -11,11 +13,29 @@ const upload = multer({ storage });
 //!
 
 
-
 /*
 Route        /
 Des          uploading the image  to S3  and then saving the file to mongo DB
 Params       none
 Access       Public
-Method       GET
+Method       post
 */
+Router.post("/", upload.single("file"), async (req, res) => {
+    try {
+        const file = req.file;
+        const bucketOptions = {
+            Bucket: "zomatomaster123467",
+            Key: file.originalname,
+            Body: file.buffer,
+            ContentType: file.mimetype,
+            ACL: "public-read"
+        };
+        
+        const uploadImage = await s3Upload(bucketOptions);
+        return res.status(400).json({ uploadImage });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+export default Router;
--- server/utils/AWS/s3.js
+++ server/utils/AWS/s3.js
@@ -1,5 +1,6 @@
 import AWS from "aws-sdk";
 
+
 const s3Bucket = new AWS.S3({
     accessKeyId:process.env.AWS_S3_accessKeyId,//!put is from .env , which should be in the package.json file , in same hierarchy
     secretAccessKey: process.env.AWS_S3_secretAccessKey,
